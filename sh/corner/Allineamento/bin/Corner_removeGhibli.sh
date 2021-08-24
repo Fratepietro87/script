e8c610a05deb1633de8ffc8bad4079659ec46ce7
Giorgio Fratepietro <82342977+Fratepietro87@users.noreply.github.com>
2021-08-24 13:41:24 +0200

    Add files via upload
    modifica per rendere il confronto case insensitive

--- sh/corner/Allineamento/bin/Corner_removeGhibli.sh
+++ sh/corner/Allineamento/bin/Corner_removeGhibli.sh
@@ -2,7 +2,7 @@
 
 #######################################
 # script per la acquisizione dei      #
-# file da gibli	via SFTP			  #
+# file da gibli via SFTP                          #
 # Autore: Giorgio Fratepietro         #
 # Società:Primeur                     #
 # Versione:1                          #
@@ -21,12 +21,12 @@ export ELENCOGHI=$ELENCOFILE\_Ghibli
 export hostremoto=172.27.131.81
 export dirwork=/opt/ghibli/corner
 #export dirinput=$dirwork/input
-export dirinput=/opt/ghibli/share/shared/temp/allineamento
-export dirlog=$dirwork/log
+export dirinput=/opt/ghibli/share/shared/temp/allineamento_cancellazione
+export dirlog=/opt/ghibli/share/logs
 export dircfg=$dirwork/cfg
 export dirbin=$dirwork/bin
 export dirdone=$dirwork/done
-
+export PORT=2622
 #log
 
 mainlog=$dirlog/Acquisitore_sftp_$USER\_$DATA.log
@@ -57,21 +57,26 @@ fi
 echo \[${unique_id}\] \[INFO\] "l'elenco dei file da confrontare esiste"     >> $mainlog
 echo \[${unique_id}\] \[INFO\] "creo il listato lato ghibli dei file per lo user: $USER"     >> $mainlog
 cd $dirwork/done
-echo "ls" | sshpass -p $PASW sftp -P 2622 $USER@$hostremoto >> $ELENCOGHI
+echo "ls" | sshpass -p $PASW sftp -P $PORT $USER@$hostremoto > $ELENCOGHI\_temp
+sed 1d $ELENCOGHI\_temp > $ELENCOGHI
 for FILE in `cat $ELENCOGHI `
 do
-	echo \[${unique_id}\] \[INFO\] "verifico l'esistenza del file $FILE su spazio" >> $mainlog
-	grep $FILE $ELENCOFILE
-	export rcgrp=$?
-	if [ $rcls1 -ne 0 ]
-	then
+        echo \[${unique_id}\] \[INFO\] "verifico l'esistenza del file $FILE su spazio" >> $mainlog
+        grep -i $FILE $ELENCOFILE
+        export rcgrp=$?
+        if [ $rcgrp -ne 0 ]
+        then
                 echo \[${unique_id}\] \[INFO\] "$FILE da cancellare" >> $mainlog
-				echo "rm $FILE" | sshpass -p $PASW sftp -P 2622 $USER@$hostremoto >> $mainlog
+                                echo "rm $FILE" | sshpass -p $PASW sftp -P $PORT $USER@$hostremoto >> $mainlog
     else
-				echo \[${unique_id}\] \[INFO\] "$FILE non è da cancellare" >> $mainlog
-	fi
+                                echo \[${unique_id}\] \[INFO\] "$FILE non è da cancellare" >> $mainlog
+        fi
 done
 echo \[${unique_id}\] \[INFO\] "ciclo di acquisizione dei file per lo user: $USER terminato"     >> $mainlog
 echo \[${unique_id}\] \[INFO\] `date "+%Od/%0m/%Oy %H:%M:%S"` - $*  >> $mainlog
 echo \[${unique_id}\] ====================END=OK=$ELENCOFILE========================================================================= >> $mainlog
+rm $ELENCOGHI\_temp
+rm $ELENCOGHI
 exit 0
+
+
